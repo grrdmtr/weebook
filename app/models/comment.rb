769874a9429252af7043a5578202afcbb7048f9a
grrdmtr <dumitrugraur@gmail.com>
2023-02-07 13:35:47 +0200

    Adds timestamps for posts and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :content, presence: true, length: { in: 1..500 }
+
+  def created_time_formatted
+    created_at.strftime("%b %-d, %Y - %l:%M %P")
+  end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :content, presence: true, length: { in: 1..500 }
+
+  def created_time_formatted
+    created_at.strftime("%b %-d, %Y - %l:%M %P")
+  end
 end
